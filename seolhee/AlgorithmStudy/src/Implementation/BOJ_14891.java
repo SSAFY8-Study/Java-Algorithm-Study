fdf9e5003129e3eabb47876ff3560ccbd6234b92
seolhee2750 <2750seolhee@naver.com>
2022-08-03 09:04:10 +0900

    [change] 공통 - #14891 문제 풀이 완료

--- seolhee/AlgorithmStudy/src/Implementation/BOJ_14891.java
+++ seolhee/AlgorithmStudy/src/Implementation/BOJ_14891.java
@@ -2,12 +2,13 @@ package Implementation;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BOJ_14891 {
 	
 	static char[][] ns = new char[4][]; // 각 기어들의 n, s극 정보
-	static int[] dirs = new int[4]; // 각 기어들이 갖게 될 회전 방향 정보
+	static int[] dirs; // 각 기어들이 갖게 될 회전 방향 정보
 	
 	// 각 톱니의 회전 방향에 맞게 회전시켜주는 메소드
 	public static void spin() {
@@ -21,7 +22,7 @@ public class BOJ_14891 {
 				ns[i][0] = (char)tmp;
 			}
 			// 반시계 방향
-			else {
+			if(dirs[i] == -1) {
 				tmp = ns[i][0];
 				for(int j = 0; j < 7; j++) ns[i][j] = ns[i][j + 1];
 				ns[i][7] = (char)tmp; 
@@ -39,6 +40,7 @@ public class BOJ_14891 {
 		for(int i = 0; i < num; i++) {
 			st = new StringTokenizer(in.readLine());
 			int tobni = Integer.parseInt(st.nextToken()) - 1; // 회전을 시작하는 톱니의 인덱스
+			dirs = new int[4];
 			dirs[tobni] = Integer.parseInt(st.nextToken());
 			
 			// 왼쪽 톱니바퀴 검사
@@ -48,10 +50,11 @@ public class BOJ_14891 {
 			}
 			// 오른쪽 톱니바퀴 검사
 			for(int j = (tobni + 1); j < 4; j++) {
-				 if(ns[j][6] == ns[j-1][2]) break;
-				 else dirs[j] = -dirs[j-1];
+				if(ns[j][6] == ns[j-1][2]) break;
+				else dirs[j] = -dirs[j-1];
 			}
 			
+			System.out.println(Arrays.toString(dirs));
 			spin(); // 각 톱니 회전
 		}
 		
@@ -65,3 +68,12 @@ public class BOJ_14891 {
 	}
 
 }
+/*
+ * 10101111
+01111101
+11001110
+00000010
+2
+3 -1
+1 1
+ */
