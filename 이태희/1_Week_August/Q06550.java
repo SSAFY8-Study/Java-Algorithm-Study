31eb63e1030c5719a5be2312d6f8955b64c56127
Malachai Lee <prussian1933@naver.com>
2022-08-03 17:43:05 +0900

    0803 - Q6550 commit

--- 이태희/1_Week_August/Q06550.java
+++ 이태희/1_Week_August/Q06550.java
@@ -22,20 +22,20 @@ public class Q06550 {
 	public static void main(String[] args) throws IOException {
 		
 		while(true) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			if (st.countTokens() != 2) break;
+			String str = br.readLine();
+			if (str == null) break;
+			//if (str.equals("")) break;
+			StringTokenizer st = new StringTokenizer(str, " ");	
 			String str1 = st.nextToken();
 			String str2 = st.nextToken();
 			int p1 = 0; int p2 = 0; boolean valid = false;
 			while(p2 < str2.length()) {
-				if(p1 == str1.length()-1) {
+				if(str1.charAt(p1) == str2.charAt(p2)) p1++;
+				p2++;
+				if(p1 >= str1.length()) {
 					valid = true;
 					break;
 				}
-				if(str1.charAt(p1) == str2.charAt(p2)) {
-					p1++;
-					p2++;
-				} else p2++;
 			}
 			if (valid) sb.append("Yes\n");
 			else sb.append("No\n");
