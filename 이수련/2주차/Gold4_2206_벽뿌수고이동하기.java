bfdcd4db9be1b0901831559d19553d9a61365c20
dolpongg <dolpong_@naver.com>
2022-08-16 16:22:59 +0900

    2주차 공통 문제 커밋 - 주석 추

--- 이수련/2주차/Gold4_2206_벽뿌수고이동하기.java
+++ 이수련/2주차/Gold4_2206_벽뿌수고이동하기.java
@@ -29,7 +29,7 @@ public class Gold4_2206_벽뿌수고이동하기 {
 		
 		go(1,1, 1, new boolean[N+1][M+1], 1);
 		
-		if(answer == 2147483647) {
+		if(answer == Integer.MAX_VALUE) {
 			System.out.println(-1);
 			return;
 		}
@@ -37,22 +37,23 @@ public class Gold4_2206_벽뿌수고이동하기 {
 	}
 	
 	
-	public static void go(int r, int c, int b, boolean [][] visited, int cnt) {
-		if (answer == M + N -1 || answer <= cnt) return;
-		if (r == N && c == M) {
+	public static void go(int r, int c, int b, boolean [][] visited, int cnt) { 
+		if (answer == M + N -1 || answer <= cnt) return; // answer가 이미 최소거나 cnt가 answe보다 큰 경우 더 진행할 필요 없
+		
+		if (r == N && c == M) { // 더 방문할 노드가 남아는 있는데 이미 목적지에 도착한 경
 			answer = Math.min(answer, cnt);
 			return;
 		}
 		
 		for(int[] delta : deltas) {
 			if(isIn(r + delta[0], c + delta[1]) && (!visited[r + delta[0]][c + delta[1]])) {
-				if((map[r + delta[0]][c + delta[1]] == 0) ) {
+				if((map[r + delta[0]][c + delta[1]] == 0) ) { // 0이면 일단 go
 					visited[r + delta[0]][c + delta[1]] = true;
 					go(r + delta[0],c + delta[1], b, visited, cnt+1);
 					visited[r + delta[0]][c + delta[1]] = false;
 				}
 				else {
-					if(b > 0) {
+					if(b > 0) {//1이면 벽 뿌수고  뿌술 수 있는 횟수 -1 해서 go
 						visited[r + delta[0]][c + delta[1]] = true;
 						go(r + delta[0],c + delta[1], b-1, visited, cnt+1);
 						visited[r + delta[0]][c + delta[1]] = false;
@@ -61,6 +62,7 @@ public class Gold4_2206_벽뿌수고이동하기 {
 				}
 			}
 		}
+		
 		//더이상 갈 곳이 없음
 		//근데 여기가 M * N인지
 		//맞으면 최솟값 바꾸기
