1faae588d2884bd2cd4ffb1a67a6ea6531859b54
angly97 <angly97@naver.com>
2022-08-18 11:40:48 +0900

    change 6593

--- 민초현/3_Week_August/BOJ_06593_상범빌딩.java
+++ 민초현/3_Week_August/BOJ_06593_상범빌딩.java
@@ -9,6 +9,7 @@ import java.util.StringTokenizer;
 
 public class BOJ_06593_상범빌딩 {
 
+	// 15880KB 132ms
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
